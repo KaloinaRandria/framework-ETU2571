6355496bc22e95b3c5d06a1a071a46ed80f60ff3
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-05-14 13:12:22 +0300

    create controller.java annotation

---
+++ src/mg/ituprom16/annotations/Controller.java
@@ -0,0 +1,12 @@
+package mg.ituprom16.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+    
+}
