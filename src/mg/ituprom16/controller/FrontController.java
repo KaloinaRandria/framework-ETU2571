13bed3a548dbba7aa2d071d5de85655f95acb52d
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-05-14 13:30:06 +0300

    Update FrontController.java
    update method processRequest

--- src/mg/ituprom16/controller/FrontController.java
+++ src/mg/ituprom16/controller/FrontController.java
@@ -8,9 +8,11 @@ import java.util.Vector;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mg.ituprom16.annotations.Controller;
 
 public class FrontController extends HttpServlet {
     String packageSource;
@@ -33,24 +35,62 @@ public class FrontController extends HttpServlet {
             if (file.isFile() && file.getName().endsWith(".class")) {
                 String className = file.getName().substring(0, file.getName().length() - 6);
                 Class<?> class1 = Thread.currentThread().getContextClassLoader().loadClass(className);
-                
+                if (class1.isAnnotationPresent(Controller.class)) {
+                    this.listeController.add(class1);
+                }
             }
         }    
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.processRequest(req, resp);
+        try {
+            this.processRequest(req, resp);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ServletException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.processRequest(req, resp);
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        try {
+            this.processRequest(req, resp);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ServletException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    protected void processRequest (HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
+    protected void processRequest (HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException, ClassNotFoundException {
         PrintWriter out = resp.getWriter();
         out.println(req.getRequestURL());
+        String print = "";
+        if (this.listeController.equals(null)) {
+            for (int i = 0; i < this.listeController.size(); i++) {
+                print += listeController.elementAt(i).getName()+"\n"; 
+            }
+        }
+        else {
+            this.getListeController();
+            for (int i = 0; i < this.listeController.size(); i++) {
+                 print += listeController.elementAt(i).getName()+"\n";               
+            }
+        }
+        out.println(print);
+        out.close();
     }
 
 } 
