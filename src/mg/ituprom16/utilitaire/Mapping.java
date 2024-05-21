3a01922807d72033b368df3f15fd2b2460ee9a2c
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-05-21 17:14:34 +0300

    Create Mapping.java

---
+++ src/mg/ituprom16/utilitaire/Mapping.java
@@ -0,0 +1,19 @@
+package mg.ituprom16.utilitaire;
+
+public class Mapping {
+    String className;
+    String methodName;
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethodName() {
+        return methodName;
+    }
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+    
+}
