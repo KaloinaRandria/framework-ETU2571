3522de4ccc9bd25dd6babd56af740d2292b633d1
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-05-23 16:19:21 +0300

    Create Utils.java

---
+++ src/mg/ituprom16/utilitaire/Utils.java
@@ -0,0 +1,9 @@
+package mg.ituprom16.utilitaire;
+
+import java.util.HashMap;
+
+public class Utils {
+    public static void scanClass(Class<?> classController , HashMap<String,Mapping> mapping) {
+        
+    }
+}
