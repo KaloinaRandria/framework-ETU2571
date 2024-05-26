475ac90a777cfe0308c68be019a38f5d7e0a4878
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-05-26 14:21:59 +0300

    update 2

--- tempJava/Controller.java
+++
@@ -1,9 +0,0 @@
-package mg.ituprom16.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Controller {
-    
-}
--- tempJava/Main.java
+++
@@ -1,8 +0,0 @@
-package mg.ituprom16;
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World ");
-    }
-}
