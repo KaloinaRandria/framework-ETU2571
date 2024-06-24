422268edc5156aa973baef1f49157ae52258bb61
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-06-24 11:14:50 +0300

    Create object Param annotation

---
+++ src/mg/ituprom16/annotations/ObjectParam.java
@@ -0,0 +1,12 @@
+package mg.ituprom16.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ObjectParam {
+    String value() default "";
+}
